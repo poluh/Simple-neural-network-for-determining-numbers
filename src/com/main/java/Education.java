6b2f206fa23137aac71cb68e51a285d09bee5111
sergey <s.poluh@ro.ru>
2018-04-07 01:04:01 +0300

    Added example for education

---
+++ src/com/main/java/Education.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Education {
+
+    private List<String> numbers = Arrays.asList(
+            "111101101101111",
+            "001001001001001",
+            "111001111100111",
+            "111001111001111",
+            "101101111001001",
+            "111100111001111",
+            "111100111101111",
+            "111001001001001",
+            "111101111101111",
+            "111101111001111");
+
+    private List<String> zero = Arrays.asList(
+            "010101101101010",
+            "111101000000111",
+            "111101101101000",
+            "111101000000111");
+    private List<String> one = Arrays.asList(
+            "001001000000001",
+            "001011101001001",
+            "001001001000001");
+    private List<String> two = Arrays.asList(
+            "111000111000111",
+            "111001010100111",
+            "101001111100101");
+    private List<String> three = Arrays.asList(
+            "111000111000111",
+            "011000011000011",
+            "111010111010111");
+    private List<String> four = Arrays.asList(
+            "001011101111001",
+            "101101111001000");
+    private List<String> five = Arrays.asList(
+            "111100111000111",
+            "111100010001111",
+            "111100011001111",
+            "110100111001111",
+            "110100111001011",
+            "111100101001111");
+    private List<String> six = Arrays.asList(
+            "111100000101111",
+            "110100111101111",
+            "000100111101111");
+    private List<String> seven = Arrays.asList(
+            "111000000001001",
+            "110001001001000",
+            "011001001001001" +
+            "111001000000001");
+    private List<String> eight = Arrays.asList(
+            "111000111000111",
+            "101101111101111",
+            "111100110101111" +
+            "111101110101111");
+    private List<String> nine = Arrays.asList(
+            "111100111001111",
+            "101101111001111",
+            "111101111000111");
+
+}
