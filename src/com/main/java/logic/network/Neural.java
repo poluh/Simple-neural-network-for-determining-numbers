f1ae63c268e2c9ee795c32efefe437abafce174e
sergey <s.poluh@ro.ru>
2018-04-06 13:45:16 +0300

    Neuron class implemented

--- src/com/main/java/logic/network/Neural.java
+++ src/com/main/java/logic/network/Neural.java
@@ -1,4 +1,24 @@
 package logic.network;
 
 public class Neural {
+
+    private double weight = 0;
+    private int input;
+
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getInput() {
+        return input;
+    }
+
+    public void setInput(int input) {
+        this.input = input;
+    }
 }
