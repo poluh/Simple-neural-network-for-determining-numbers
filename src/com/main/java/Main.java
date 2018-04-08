c8ff420ec0237f2857cdd2f72e53c120fc3b3033
sergey <s.poluh@ro.ru>
2018-04-08 23:47:54 +0300

    Added test sample for all numbers

--- src/com/main/java/Main.java
+++ src/com/main/java/Main.java
@@ -2,7 +2,7 @@ import logic.network.Network;
 
 public class Main {
     public static void main(String[] args) {
-        Network network = new Network("001001001001001");
+        Network network = new Network("011101111001111");
         System.out.println(network.getResult());
     }
 }
--- tests/network/NetworkTest.java
+++ tests/network/NetworkTest.java
@@ -23,11 +23,63 @@ class NetworkTest {
                     "111101111101111",
                     "111101111001111");
 
+    private static List<String> zero = Arrays.asList(
+            "010101101101010",
+            "111101000000111",
+            "111101101101000",
+            "111101000000111");
+    private static List<String> one = Arrays.asList(
+            "001001000000001",
+            "001011001001001",
+            "001001001000001");
+    private static List<String> two = Arrays.asList(
+            "111001010100111",
+            "111001011100110",
+            "101001111100101");
+    private List<String> three = Arrays.asList(
+            "011001111001011",
+            "111010111001111");
+    private List<String> four = Arrays.asList(
+            "001101111001001",
+            "101101111001000");
+    private static List<String> five = Arrays.asList(
+            "111100111000111",
+            "111100010001111",
+            "110100111001111",
+            "110100111001011");
+    private List<String> six = Arrays.asList(
+            "111000111101111",
+            "111100110101111",
+            "000100111101111");
+    private List<String> seven = Arrays.asList(
+            "111000000001001",
+            "110001001001000");
+    private List<String> eight = Arrays.asList(
+            "111001111101111",
+            "101101111101111",
+            "111101110101111");
+    private List<String> nine = Arrays.asList(
+            "011101111001111",
+            "111101111001111",
+            "111101111001001");
+
+    private List<List<String>> testSample = Arrays.asList(zero, one, two, three, four, five, six, seven, eight, nine);
+
     @Test
     void checkNormalNumber() {
         for (int i = 0; i < 10; i++) {
             Network network = new Network(normalNumbers.get(i));
             assertEquals(i, network.getResult());
+        }
+    }
+
+    @Test
+    void testSample() {
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            testSample.get(i).forEach(number -> {
+                assertEquals(finalI, new Network(number).getResult());
+            });
         }
     }
 
