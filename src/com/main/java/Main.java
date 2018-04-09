66048ddeccf0ebb3236d8492bb8ef8a554f9c4cf
sergey <s.poluh@ro.ru>
2018-04-09 03:38:10 +0300

    Added class for process image

--- src/com/main/java/Main.java
+++ src/com/main/java/Main.java
@@ -1,8 +1,10 @@
+import logic.image.ImageIOManager;
 import logic.network.Network;
 
 public class Main {
     public static void main(String[] args) {
         Network network = new Network("011101111001111");
         System.out.println(network.getResult());
+        ImageIOManager imageIOManager = new ImageIOManager("/Users/sergey/Desktop/Untitled.png");
     }
 }
---
+++ src/com/main/java/logic/image/ImageIOManager.java
@@ -0,0 +1,48 @@
+package logic.image;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImageIOManager {
+
+    private int[] numberMatrix = new int[15];
+    private List<BufferedImage> subImages = new ArrayList<>();
+
+    public ImageIOManager(String path) {
+        File imageFile = new File(path);
+        BufferedImage bufferedImage = null;
+        try {
+            bufferedImage = ImageIO.read(imageFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assert bufferedImage != null;
+        int subHeight = bufferedImage.getHeight() / 5;
+        int subWidth = bufferedImage.getWidth() / 3;
+        System.out.println(subHeight + " " + subWidth);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                subImages.add(bufferedImage.getSubimage(i * subWidth, j * subHeight, subWidth, subHeight));
+            }
+        }
+
+        final int[] i = {0};
+        subImages.forEach(image -> {
+            try {
+                ImageIO.write(image, "PNG", new File("image" + i[0]));
+                i[0]++;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+
+    public int[] getNumberMatrix() {
+        return numberMatrix;
+    }
+}
