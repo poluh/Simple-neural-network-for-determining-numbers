b2315fb9292e6132f7fbca83ec914ca9f8cba853
sergey <s.poluh@ro.ru>
2018-04-14 02:37:55 +0300

    Small addition of Education

--- src/com/main/java/logic/network/education/Education.java
+++ src/com/main/java/logic/network/education/Education.java
@@ -1,14 +1,18 @@
 package logic.network.education;
 
-import logic.network.Layer;
-import logic.network.Network;
-import logic.network.Neuron;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import logic.image.ImagePreprocessor;
 
 public class Education {
+
+    private double[][] imagesSignals = new double[10][15];
+    private ImagePreprocessor[] imagePreprocessors = new ImagePreprocessor[10];
+    private String commonPath = "src/com/main/java/logic/network/education/imageForEducation/";
+
+    Education() {
+        for (int i = 0; i < 10; i++) {
+            imagePreprocessors[i] = new ImagePreprocessor(commonPath + i + ".jpg", 15);
+            imagesSignals[i] = imagePreprocessors[i].getImageSignals();
+        }
+    }
 
 }
