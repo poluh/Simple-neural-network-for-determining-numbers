c5c0a3ac2d44b318a3298a74f26b1cb9b1dcb3af
sergey <s.poluh@ro.ru>
2018-04-14 02:27:45 +0300

    Added images for training

---
+++ src/com/main/java/logic/network/education/Education.java
@@ -0,0 +1,14 @@
+package logic.network.education;
+
+import logic.network.Layer;
+import logic.network.Network;
+import logic.network.Neuron;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Education {
+
+}
